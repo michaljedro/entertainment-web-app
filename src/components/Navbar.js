397634f02bf80927d985a76391b8d6204a88f602
michaljedro <michal.jedro@wp.pl>
2022-06-14 23:28:13 +0200

    im creating style in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
 import React from "react";
+import styled from "styled-components";
 const Navbar = () => {
   return <div>Navbar</div>;
 };
 
 export default Navbar;
+
+const Navbar = styled.div``;
