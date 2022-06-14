d662bd2ff8c25e147320f90d65a6178cc236089a
michaljedro <michal.jedro@wp.pl>
2022-06-14 23:09:57 +0200

    add constants links with icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,9 @@
 import { StyledHeader, Nav, Logo, Image } from "./styles/Header.styled";
 
 export default function Header() {
-  return <StyledHeader>hEADER</StyledHeader>;
+  return (
+    <StyledHeader>
+      <h1>Header</h1>
+    </StyledHeader>
+  );
 }
---
+++ src/utils/consants.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { FaBookmark, FaWindows, MdLocalMovies } from "react-icons/fa";
+
+export const links = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "about",
+    url: "/about",
+  },
+  {
+    id: 3,
+    text: "products",
+    url: "/products",
+  },
+];
